3510f973451b35ca57aa14963ca23df131787a3a
Maria Trezha Von Bargen <vonbargen.trezha@gmail.com>
2026-09-01 23:49:28 -0700

    Lab-1-Hello-World-Java
    A simple Hello World Java program.

---
+++ CS1400/HelloWorld.java
@@ -0,0 +1,11 @@
+// Name: Maria Trezha Von Bargen
+// Course: CS 1400-02
+// Professor Posadas
+// September 1, 2026
+
+class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
